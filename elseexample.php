2f6ff8a9b47757980ca2f48666c289c9e7d6644a
Warlune <Warlune1@gmail.com>
2014-10-03 11:35:56 -0500

    added 2 more examples . elseexample.php

--- elseexample.php
+++ elseexample.php
@@ -1,17 +1,37 @@
 <?php
 
-$isRaining = true;
-$haveMoney = true;
+// $isRaining = true;
+// $haveMoney = true;
 
-if ($isRaining) {
-	echo "yay it is raining ... do i have money\n";
+// if ($isRaining) {
+// 	echo "yay it is raining ... do i have money\n";
 
-	if ($haveMoney) {
-		echo "I'm buying a new car!\n";
-	} else {
-		echo 'we broke';
-	}
+// 	if ($haveMoney) {
+// 		echo "I'm buying a new car!\n";
+// 	} else {
+// 		echo 'we broke';
+// 	}
 
-} else {
-	echo 'not even raining man';
-}
+// } else {
+// 	echo 'not even raining man';
+// }
+//=======================================================================
+// $age = 22;
+// if($age >= 18) {
+// 	$isAdult = true;
+// } else {
+// 	$isAdult = false;
+// }
+
+// both are the same ^^
+
+// $age = 17;
+// $isAdult = $age >= 18 ? 'can watch movie' : 'go home youngin';
+// echo "$isAdult\n";
+
+$hasFuel = true
+
+echo ($hasFuel) ? 'car will run' : 'take the bus';
+
+
+
