baac6ef20b7f4c2bf4d875d769e378309a04db47
Warlune <Warlune1@gmail.com>
2014-10-05 13:26:10 -0500

    loop example

---
+++ loopexample.php
@@ -0,0 +1,34 @@
+<?php
+
+$a = 6;
+do {
+	echo "\$a is equal to {$a}\n";
+	$a++;
+} while ($a <= 5);
+
+while ($a <= 5) {
+	echo "\$a is equal to {$a}\n";
+	$a++;
+}
+
+$a = 1;
+do {
+	echo "\$a is equal to {$a}\n";
+	$a++;
+	while (1 != 2)
+}
+
+// DO WHILE LOOP
+
+<?php
+
+$count =1;
+do {
+	echo `say $count . ' bat .. ah ah'`;
+	$count++;
+} while($count <= 5);
+
+sleep(1);
+
+echo `say 'we have ' . $count 'bats....... ahahahahahahahahah'`;
+echo `say 'thunder.... boom'`;
