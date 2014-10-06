d14ae90bbdf2097a4c4137606dcf0a2625b5b56f
Warlune <Warlune1@gmail.com>
2014-10-06 09:16:36 -0500

    added comments

--- elseifsolo.php
+++ elseifsolo.php
@@ -1,20 +1,20 @@
 <?php
-
+// my constants
 define('SIDES_OF_DICE', 50);
 $bool = ((bool) rand(0,1));
 define('YES_NO' , $bool);
 define('MONEY', 150);
-
-
+//random user data
 $anthony = [
 	    'age' => mt_rand(1, SIDES_OF_DICE),
 	    'human' => YES_NO,
 	    'cash' => mt_rand(1, SIDES_OF_DICE)
 ];
 
-
+//checking if human or robot
 if ($anthony['human']) {
 	echo "you are a human\n";
+	//changes variable $answer to be checked later
 	 $answer = "yes";
 
 } else {
@@ -32,15 +32,17 @@ do {
 } while($count <= 5);
 
 sleep(1);
-
+// checking $answer and verifying your age 
 if (($answer === "yes") && ($anthony['age'] >= 18)) {
 	echo "you are a human and you are of age\n";
+	//making variable to be checked later
 	($welcome = true);
 	($rwelcome = 10);
 } elseif (($answer === "yes") && ($anthony['age'] <= 18)) {
 	echo "you are a human and you are not of age\n";
 } elseif (($answer === "no") && ($anthony['age'] >= 18)) {
 	echo "you are a robot and you are of age\n";
+	//making variable to be checked later
 	($rwelcome = true);
 	($welcome = 10);
 } elseif (($answer === "no") && ($anthony['age'] <= 18)) {
@@ -60,19 +62,24 @@ do {
 } while($count <= 5);
 
 sleep(1);
-
+//checks for the created variables $welcome and $rwelcome and their values
 if (($welcome == true) && ($rwelcome === 10)) {
 	echo "welcome to the resturant we have a fine selection foods\n";
+	//creates a $hum to check later
 		$hum = true;
 } elseif (($welcome == false) && ($rwelcome === 10)) {
 	echo "I am sorry you are not old enough to eat at this place\n";
+	//checks welcome variables errors to the else statement
 } elseif (($rwelcome == true) && ($welcome === 10)) {
 	echo "Welcome to the bolts and oil store\n";
+	//creates a $rob to check later
 		$rob = true;
 } elseif (($rwelcome == false) && ($welcome === 10)) {
 	echo "I am sorry you are not able to access this facility\n";
+	// errors to else
 } else {
 	echo "You are not welcome please return when you are of age\n";
+	// changes inital cash to false
 	($anthony['cash'] = false);
 }
 
@@ -87,7 +94,7 @@ do {
 } while($count <= 5);
 
 sleep(1);
-
+//multi-dim array for the menu
 $menu = [
 
 	'hmenu' => [
@@ -126,15 +133,16 @@ $menu = [
 	 ],
 	]
 ];
-
+//checks if your cash has value
 if ($anthony['cash'] == true){
+	// checks for $hum/$rob and if your money is enough for more expensive food
 	if (($hum == true) && ($anthony['cash'] >= 1) || ($anthony['cash'] <= 13)) {
 		echo "here is our cheap menu\n";
 		print_r($menu['hmenu']['cheap']);
 	} elseif (($hum == true) && ($anthony['cash'] >= 14) || ($anthony['cash'] <= 126)) {
 		echo "here is our moderate menu\n";
 		print_r($menu['hmenu']['moderate']);
-	} elseif (($hum == true) && ($anthony['cash'] >= 126) || ($anthony['cash'] <= 134)) {
+	} elseif (($hum == true) && ($anthony['cash'] >= 126) || ($anthony['cash'] <= 150)) {
 		echo "Here are our luxuary items\n";
 		print_r($menu['hmenu']['expensive']);
 	} elseif (($rob == true) && ($anthony['cash'] >= 1) || ($anthony['cash'] <= 13)) {
@@ -143,12 +151,13 @@ if ($anthony['cash'] == true){
 	} elseif (($rob == true) && ($anthony['cash'] >= 14) || ($anthony['cash'] <= 126)) {
 		echo "here are some of out bigger scraps of metal\n";
 		print_r($menu['rmenu']['moderate']);
-	} elseif (($rob == true) && ($anthony['cash'] >= 127) || ($anthony['cash'] <= 134)) {
+	} elseif (($rob == true) && ($anthony['cash'] >= 127) || ($anthony['cash'] <= 150)) {
 		echo "here are our rare metals\n";
 		print_r($menu['rmenu']['expensive']);
 	} else {
 		echo " you are not authorized to be here or see the menu\n";
 	}
+// doesnt show the menu to people not old enough
 } else {
 	echo "     \n";
 }
